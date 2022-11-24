ed4522232ce46f467238423b7368ef16a6538f49
sebas_satch <sebas.sigales@gmail.com>
2022-11-23 22:02:57 -0300

    entregalista

---
+++ chemevoy/src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import '../ItemListContainer/ItemListContainer.css'
+
+const ItemListContainer = ({greeting}) => {
+    return (
+        <h2 className="saludo"> {greeting} </h2>
+    )    
+}
+
+export default ItemListContainer
+
+
---
+++ chemevoy/src/components/Title/Title.jsx
@@ -0,0 +1,10 @@
+
+const Title = (props) => {
+    return (
+        <div>
+            <h1> {props.encabezado} </h1>
+        </div>
+    )    
+}
+
+export default Title
